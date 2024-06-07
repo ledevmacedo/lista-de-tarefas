5af7cfbc73dd5394ec02d72c2924235edd9eefde
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-06-07 10:18:23 +0100

    to-do

--- to-do/src/Tarefas.jsx
+++ to-do/src/Tarefas.jsx
@@ -1,9 +1,11 @@
 export default function Tarefas (){
     return(
         <div className="flex justify-center mt-10 ">
-            <div className="w-screen max-w-2xl h-12 bg-gray-400 flex items-center px-4">
-            <button className="bg-white">x</button><p className="text-white">estudar programação, ir para a faculdade</p>
+            <div className=" w-screen max-w-2xl h-12 bg-gray-400 flex items-center   px-4">
             <button className="bg-white">x</button>
+            <p className="text-white px-40">estudar programação, ir para a faculdade</p>
+            <button className="bg-white ">x</button>
+            <h1>teste</h1>
         </div>
     </div>
     )
