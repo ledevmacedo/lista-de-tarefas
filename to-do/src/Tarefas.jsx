d6fb316fff35725b08b575e38240edc7ca0fdb24
Caroline Tenorio <130055209+Carolthe@users.noreply.github.com>
2024-06-09 19:55:51 +0100

    to-do

--- to-do/src/Tarefas.jsx
+++ to-do/src/Tarefas.jsx
@@ -18,9 +18,9 @@ export default function Tarefas (props){
 
         <div className="flex justify-center  mt-10 ">
             <div className=" w-screen max-w-2xl h-12 bg-gray-400 flex items-center px-4 justify-between">
-            <button className="bg-white">x</button>
+            <button className="bg-gray-500 text-white w-6 rounded-full  ">.</button>
             <p className="text-white px-4 w-full">{tarefaRecebida.nomeTarefa}</p>
-            <button className="bg-white ">x</button>
+            <button className="bg-gray-500 text-white w-6 rounded-full ">.</button>
            
         </div>
     </div>
